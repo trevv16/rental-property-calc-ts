2f2b99c1dccbd06819b933e84cff195773a9209c
trevv16 <tnjeru16@gmail.com>
2021-03-22 12:32:10 -0400

    fixed props typo error

--- src/views/review/ReviewInfo.tsx
+++ src/views/review/ReviewInfo.tsx
@@ -7,11 +7,11 @@ export default function ReviewInfo(props: any) {
             <div className="prose sm:prose-xl">
                 <div className="info">
                     <h3 className="m-2">Info</h3>
-                    <h5>{`Property Name: ${props.infoname}`}</h5>
-                    <h5>{`Street Address: ${props.infostreetAddress}`}</h5>
-                    <h5>{`City: ${props.infocity}`}</h5>
-                    <h5>{`State: ${props.infostate}`}</h5>
-                    <h5>{`Zip Code: ${props.infozipCode}`}</h5>
+                    <h5>{`Property Name: ${props.info.name}`}</h5>
+                    <h5>{`Street Address: ${props.info.streetAddress}`}</h5>
+                    <h5>{`City: ${props.info.city}`}</h5>
+                    <h5>{`State: ${props.info.state}`}</h5>
+                    <h5>{`Zip Code: ${props.info.zipCode}`}</h5>
                 </div>
             </div>
         </div>
