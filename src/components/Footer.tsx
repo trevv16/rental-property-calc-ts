4d3c7749cae463cb73c2296fb6030a6abb155629
trevv16 <tnjeru16@gmail.com>
2021-03-22 17:58:44 -0400

    updated github link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
 				<div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
 					<div className="flex justify-center space-x-6 md:order-2">
 						<a
-							href="https://github.com/trevv16"
+							href="https://github.com/trevv16/rental-property-calc-ts"
 							className="text-gray-400 hover:text-gray-500"
 							rel="noopener noreferrer"
 							target="_blank"
