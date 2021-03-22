cb7411732abdf0d3040413bf5b32198413937b05
trevv16 <tnjeru16@gmail.com>
2021-03-22 15:09:14 -0400

    cleaned up props

--- src/views/review/ReviewLoan.tsx
+++ src/views/review/ReviewLoan.tsx
@@ -1,16 +1,32 @@
 import React from "react";
+import Humanize from "humanize-plus";
 
 export default function ReviewLoan(props: any) {
+	// ReviewLoan
+	const cleanLoanAmount = !isNaN(props.loan.loanAmount) ? Humanize.formatNumber(
+		props.loan.loanAmount, 2) : Humanize.formatNumber(0, 2);
+
+	const cleanInterestRate = !isNaN(props.loan.interestRate) ? Humanize.formatNumber(
+		props.loan.interestRate, 2) : Humanize.formatNumber(0, 2);
+
+	const cleanPointsCharged = !isNaN(props.loan.pointsCharged) ? Humanize.formatNumber(
+		props.loan.pointsCharged, 2) : Humanize.formatNumber(0, 2);
+
+	const cleanLoanTerm = isNaN(props.loan.loanTerm) ? 0 : props.loan.loanTerm;
+
+	const isCashPurchase = (): string => {
+		return props.loan.isCashPurchase ? "Yes" : "No";
+	};
 
 	return (
         <div>
             <div className="loan">
                 <h3 className="m-2">Loan</h3>
-                <h5>{`Cash Purchase?: ${props.isCashPurchase}`}</h5>
-                <h5>{`Loan Amount: $${props.cleanLoanAmount}`}</h5>
-                <h5>{`Interest Rate: ${props.cleanInterestRate}%`}</h5>
-                <h5>{`Points Charged: ${props.cleanPointsCharged}%`}</h5>
-                <h5>{`Loan Term: ${props.cleanLoanTerm} Years`}</h5>
+                <h5>{`Cash Purchase?: ${isCashPurchase}`}</h5>
+                <h5>{`Loan Amount: $${cleanLoanAmount}`}</h5>
+                <h5>{`Interest Rate: ${cleanInterestRate}%`}</h5>
+                <h5>{`Points Charged: ${cleanPointsCharged}%`}</h5>
+                <h5>{`Loan Term: ${cleanLoanTerm} Years`}</h5>
             </div>
         </div>
     );
--- src/views/review/ReviewPurchase.tsx
+++ src/views/review/ReviewPurchase.tsx
@@ -1,16 +1,29 @@
 import React from "react";
+import Humanize from "humanize-plus";
 
 export default function ReviewPurchase(props: any) {
+	// ReviewPurchase
+	const cleanPurchasePrice = !isNaN(props.purchase.purchasePrice) ? Humanize.formatNumber(
+		props.purchase.purchasePrice, 2) : Humanize.formatNumber(0, 2);
+
+	const cleanClosingCost = !isNaN(props.purchase.closingCost) ? Humanize.formatNumber(
+		props.purchase.closingCost, 2) : Humanize.formatNumber(0, 2);
+
+	const cleanRehabCost = !isNaN(props.purchase.rehabCost) ? Humanize.formatNumber(
+		props.purchase.rehabCost, 2) : Humanize.formatNumber(0, 2);
+
+	const cleanValueGrowth = !isNaN(props.purchase.propertyValueGrowth) ? Humanize.formatNumber(
+		props.purchase.propertyValueGrowth, 2) : Humanize.formatNumber(0, 2);
 
 	return (
         <div>
             <div className="prose sm:prose-xl">
                 <div className="purchase">
                     <h3 className="m-2">Purchase</h3>
-                    <h5>{`Purchase Price: $${props.cleanPurchasePrice}`}</h5>
-                    <h5>{`Closing Costs: $${props.cleanClosingCost}`}</h5>
-                    <h5>{`Rehab Costs: $${props.cleanRehabCost}`}</h5>
-                    <h5>{`Property Value Growth: ${props.cleanValueGrowth}%`}</h5>
+                    <h5>{`Purchase Price: $${cleanPurchasePrice}`}</h5>
+                    <h5>{`Closing Costs: $${cleanClosingCost}`}</h5>
+                    <h5>{`Rehab Costs: $${cleanRehabCost}`}</h5>
+                    <h5>{`Property Value Growth: ${cleanValueGrowth}%`}</h5>
                 </div>
             </div>
         </div>
