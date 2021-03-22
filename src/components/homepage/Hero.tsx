7148048e7d6c1a960994f09dbc04247466b363d4
trevv16 <tnjeru16@gmail.com>
2021-03-22 18:53:15 -0400

    fixed hompage img

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -78,7 +78,7 @@ export default function Hero() {
 				<div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
 					<img
 						className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-						src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixqx=wn9CjWb4yA&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+						src="https://user-images.githubusercontent.com/32273310/112067218-16b31380-8b3e-11eb-83ea-90193af5b27d.png"
 						alt=""
 					/>
 				</div>
