15c065b27e981aa928b21d6d120de0653943e32e
trevv16 <tnjeru16@gmail.com>
2021-03-22 13:09:59 -0400

    fixed property header color error

--- src/views/results/PropertyHeader.tsx
+++ src/views/results/PropertyHeader.tsx
@@ -4,14 +4,14 @@ export default function PropertyHeader(props: any) {
 
 	return (
         <div>
-            <div className="md:flex md:items-center md:justify-between">
-                <div className="flex-1 min-w-0">
-                    <h2 className="text-2xl font-bold leading-7 text-white sm:text-3xl sm:truncate">
+            <div className="text-center mt-16">
+                <div className="">
+                    <h1 className="text-2xl font-bold leading-7 sm:text-3xl sm:truncate">
                         {props.info.name}
-                    </h2>
+                    </h1>
                 </div>
-                <div className="mt-4 flex md:mt-0 md:ml-4">
-                    <h4 className="text-2xl font-bold leading-7 text-white sm:text-3xl sm:truncate">
+                <div className="">
+                    <h4 className="text-2xl font-bold leading-7 sm:text-3xl sm:truncate">
                         {`${props.info.streetAddress} ${props.info.city}, ${props.info.state} ${props.info.zipCode}`}
                     </h4>
                 </div>
