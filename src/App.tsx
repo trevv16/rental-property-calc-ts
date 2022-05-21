083e0ec14d59e7f4831aaad208f77dbc577da6e2
trevv16 <tnjeru16@gmail.com>
2022-05-21 13:16:13 -0400

    remove unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import { HomePage, CalculatorPage } from "./views/index";
-
 import { Footer } from "./components/index";
 
 function App() {
-	return (
-		<div>
-			<Router>
-				<Switch>
-					<Route exact path="/" component={HomePage} />
-					{/* <Route exact path="/faq" component={FAQPage} /> */}
-					<Route
-						exact
-						path="/calculator"
-						component={CalculatorPage}
-					/>
-				</Switch>
-			</Router>
-			<Footer />
-		</div>
-	);
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          {/* <Route exact path="/faq" component={FAQPage} /> */}
+          <Route exact path="/calculator" component={CalculatorPage} />
+        </Switch>
+      </Router>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
