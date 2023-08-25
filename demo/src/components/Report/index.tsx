1696334e25202981c52891b4e6baad690ef2527e
trevv16 <tnjeru16@gmail.com>
2023-08-25 15:11:37 -0400

    added annual/monthly toggle to report section

--- demo/src/components/Report/index.tsx
+++ demo/src/components/Report/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Switch } from "@headlessui/react";
 import { Deal } from "../../lib/deal";
 import PropertyHeader from "./PropertyHeader";
 import { Divider, Graph } from "..";
@@ -7,6 +8,10 @@ import KPISection from "./KPISection";
 import { useAmmortization } from "../../hooks/useAmmortiziation";
 import CashFlowSection from "./CashFlowSection";
 import ExpenseBreakdown from "./ExpenseBreakdown";
+
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(" ");
+}
 
 type ReportProps = {
   deal: Deal;
@@ -22,6 +27,36 @@ export default function Report({ deal }: ReportProps) {
       <>
         <PropertyHeader property={deal.property} />
         <Divider />
+        <div className="mt-8 pb-8 px-4 sm:px-6 lg:px-8">
+          <div className="sm:flex sm:items-center">
+            <div className="sm:flex-auto"></div>
+            <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+              <Switch.Group as="div" className="flex items-center">
+                <Switch
+                  checked={units === "annual"}
+                  onChange={(value) =>
+                    value ? setUnits("annual") : setUnits("monthly")
+                  }
+                  className={classNames(
+                    units === "annual" ? "bg-indigo-600" : "bg-gray-200",
+                    "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2"
+                  )}
+                >
+                  <span
+                    aria-hidden="true"
+                    className={classNames(
+                      units === "annual" ? "translate-x-5" : "translate-x-0",
+                      "pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
+                    )}
+                  />
+                </Switch>
+                <Switch.Label as="span" className="ml-3 text-sm">
+                  <span className="font-medium text-gray-900">Annual</span>
+                </Switch.Label>
+              </Switch.Group>
+            </div>
+          </div>
+        </div>
         <h2 className="my-8 text-lg leading-6 font-medium text-gray-900">
           <strong>KPIs</strong>
         </h2>
